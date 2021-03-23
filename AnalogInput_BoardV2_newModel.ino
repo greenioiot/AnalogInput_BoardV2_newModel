4ca90e97004f9c5500c49b46a7740e71196056a0
greenioiot <47623362+greenioiot@users.noreply.github.com>
2021-03-23 07:50:57 +0700

    update to connect NB-IoT

--- AnalogInput_BoardV2_newModel.ino
+++ AnalogInput_BoardV2_newModel.ino
@@ -1,43 +1,127 @@
+
+
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
- Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-//Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
+#include "HardwareSerial_NB_BC95.h"
 
-void setup(void) 
+//#include <ArduinoJson.h>
+
+
+String serverIP = "103.27.203.83"; // Your Server IP;
+String serverPort = "9956"; // Your Server Port;
+String json = "";
+
+
+
+//BluetoothSerial SerialBT;
+
+HardwareSerial_NB_BC95 AISnb;
+
+const long interval = 20000;  //millisecond
+unsigned long previousMillis = 0;
+
+signal meta ;
+
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
+int16_t adc0, adc1, adc2, adc3;
+void _init() {
+
+
+
+  Serial.println("Start...");
+
+  AISnb.setupDevice(serverPort);
+
+  String ip1 = AISnb.getDeviceIP();
+  delay(1000);
+
+  pingRESP pingR = AISnb.pingIP(serverIP);
+
+
+  String nccid = AISnb.getNCCID();
+  Serial.print("nccid:");
+  Serial.println(nccid);
+
+
+
+
+}
+void setup(void)
 {
   Serial.begin(115200);
   Serial.println("Hello!");
-  
-  Serial.println("Getting single-ended readings from AIN0..3");
-  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
-  
-  // The ADC input range (or gain) can be changed via the following
-  // functions, but be careful never to exceed VDD +0.3V max, or to
-  // exceed the upper and lower limits if you adjust the input range!
-  // Setting these values incorrectly may destroy your ADC!
-  //                                                                ADS1015  ADS1115
-  //                                                                -------  -------
-//   ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  
+  //  SerialBT.begin("AnalogInput_BoardV2"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  _init();
   ads.begin();
+  AISnb.debug = false;
+
+
+  previousMillis = millis();
+
 }
 
-void loop(void) 
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
-  int16_t adc0, adc1, adc2, adc3;
+  if ((in_max - in_min) + out_min != 0) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  } else {
+    return 0;
+  }
+}
 
-  adc0 = ads.readADC_SingleEnded(0);
-  adc1 = ads.readADC_SingleEnded(1);
-  adc2 = ads.readADC_SingleEnded(2);
-  adc3 = ads.readADC_SingleEnded(3);
+void loop(void)
+{
 
-//    Serial.print(adc0); Serial.print(" ");  Serial.print(adc1); Serial.print(" ");   Serial.print(adc2); Serial.print(" ");  Serial.println(adc3);
- int val = map(adc0, 1936, 10752, 4, 20);
-   Serial.println(val);
+
+
+
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    adc0 = ads.readADC_SingleEnded(0);
+    adc1 = ads.readADC_SingleEnded(1);
+    adc2 = ads.readADC_SingleEnded(2);
+    adc3 = ads.readADC_SingleEnded(3);
+
+    Serial.print(adc0); Serial.print(" ");  Serial.print(adc1); Serial.print(" ");   Serial.print(adc2); Serial.print(" ");  Serial.println(adc3);
+    float val0 = mapfloat(adc0, 2128, 10560, 4, 20);
+    float val1 = mapfloat(adc1, 2128, 10560, 4, 20);
+    float val2 = mapfloat(adc2, 2128, 10560, 4, 20);
+    float val3 = mapfloat(adc3, 2128, 10560, 4, 20);
+
+    meta = AISnb.getSignal();
+    String udpData2 = "{\"Tn\":\"vpbHN0ulNCCptps6qRAk\",\"adc0\":";
+    udpData2.concat(val0);
+    udpData2.concat(",\"adc1\":");
+    udpData2.concat(val1);
+    udpData2.concat(",\"adc2\":");
+    udpData2.concat(val2);
+    udpData2.concat(",\"adc3\":");
+    udpData2.concat(val3);
+    udpData2.concat(",\"rssi\":");
+    udpData2.concat(meta.rssi);
+    udpData2.concat("}");
+    Serial.println(udpData2);
+
+    UDPSend udp = AISnb.sendUDPmsgStr(serverIP, serverPort, udpData2);
+
+    previousMillis = currentMillis;
+ 
+
+    UDPReceive resp = AISnb.waitResponse();
+    AISnb.receive_UDP(resp);
+    Serial.print("waitData:");
+    Serial.println(resp.data);
+
+
+    previousMillis = currentMillis;
+  }
+
+
+
 }
